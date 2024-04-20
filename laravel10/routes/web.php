bd81415d1933a55eba63bbff32ec7b50f467fc4f
Aleksandr <sh17aleksandr@gmail.com>
2024-04-20 18:54:24 +0300

    add category Product

--- laravel10/routes/web.php
+++ laravel10/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/{cat}/{product_id}', [ProductController::class, 'show'])->name('showProduct');
+
+// Route::get('/{cat}/{product_id}', [HomeController::class, 'index']);
 
 
 // Route::get('/', function () {
