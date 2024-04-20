b5e0a832a8d76e47f85ed99b785bf59fa10accae
Aleksandr <sh17aleksandr@gmail.com>
2024-04-20 05:09:44 +0300

    before going to the main page

--- laravel10/app/Http/Controllers/HomeController.php
+++ laravel10/app/Http/Controllers/HomeController.php
@@ -11,11 +11,11 @@ class HomeController extends Controller
     {
 
         $products=Product::all();
-        foreach($products as $product){
-            echo '<br>Title: ' . $product->title. ' - Price: ' . $product->price;
-            echo '<br>---------';
-        }
-        // $products = Product::where('id', 3)->first();
+        // foreach($products as $product){
+        //     echo '<br>Title: ' . $product->title. ' - Price: ' . $product->price;
+        //     echo '<br>---------';
+        // }
+        $products = Product::where('id', 3)->first();
         // dd($products);
 
         return view('home.index');
